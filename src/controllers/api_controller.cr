489d217fd3abadeecc8bb4ee6f0ff7f3738682e1
samgarasx <samgarasx@gmail.com>
2018-09-05 18:19:39 +0200

    Remove locales

--- src/controllers/api_controller.cr
+++ src/controllers/api_controller.cr
@@ -1,7 +1,7 @@
 class ApiController < ApplicationController
   def index
     respond_with do
-      text I18n.translate "welcome_amber_webapi"
+      text "Welcome to Amber Web API"
     end
   end
 end
