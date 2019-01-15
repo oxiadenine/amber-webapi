44cb542c4f376305310e7023326fea88f4ce8d17
samgarasx <samgarasx@gmail.com>
2019-01-15 12:01:26 +0100

    Update Fruit database

--- src/entities/fruit_entity.cr
+++ src/entities/fruit_entity.cr
@@ -9,8 +9,6 @@ class FruitEntity < Granite::Base
 
   validate_not_nil :no
   validate_uniqueness :no
-  validate_max_length :no, 20
 
   validate_not_nil :description
-  validate_max_length :description, 50
 end
